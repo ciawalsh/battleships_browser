553d918bffbea705e9ec50a17129f7f8de68d3ee
bibiana <bcristofol@gmail.com>
2015-01-08 19:05:43 +0000

    Added hit/missed functionality - place ship coord/direction - hit test to be checked

--- features/step_definitions/starting_a_game_steps.rb
+++ features/step_definitions/starting_a_game_steps.rb
@@ -1,25 +1,17 @@
 When(/^I enter my name "(.*?)"$/) do |name|
-  
 	fill_in('name', with: 'Charlie')
-
 end
 
 When(/^I enter my second namep2 "(.*?)"$/) do |name|
-  
 	fill_in('namep2', with: 'Bibiana')
-
 end
 
 When(/^I enter my namep(\d+) "(.*?)"$/) do |name, number|
-
 	fill_in('namep2', with: 'Bibiana')
-
 end
 
 When(/^I don't fill my name$/) do
-
   fill_in('name', with: '')
-
 end
 
 Given(/^I fill in "(.*?)" and "(.*?)"$/) do |name, namep2|
@@ -33,13 +25,15 @@ Given(/^I press "(.*?)" and "(.*?)"$/) do |submit, submitp2|
 end
 
 When(/^I enter my coordinate "(.*?)"$/) do |ship|
-  
 	fill_in('ship', with: 'A1')
-
 end
 
-Then(/^I should see 'You MISSED!'$/) do
-  expect(page).to have_content('You MISSED!')
+Then(/^I should see 'You missed'$/) do
+  expect(page).to have_content('You missed')
+end
+
+Then(/^I place a ship in "(.*?)"$/) do |ship|
+  expect(page).to have_content('ship')
 end
 
 Then(/^I should see 'Hit!'$/) do
